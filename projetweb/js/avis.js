4cfbf2fd3f41356bb3e0a19a616eb70d600231ee
Fayçal <faycalkordjani@gmail.com>
2024-05-29 09:00:01 +0200

    Change num 3000000

---
+++ projetweb/js/avis.js
@@ -0,0 +1,7 @@
+function afterSubmit() {
+    const img = document.getElementById("aftersubmit");
+    const form = document.getElementById("formular");
+    form.style.display = "none";
+    img.style.display = "block";
+
+}
