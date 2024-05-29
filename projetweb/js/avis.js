3accb1530dcb137bd53d2f1ff6ef1e6305837405
Fayçal <faycalkordjani@gmail.com>
2024-05-29 12:27:19 +0200

    changement final

--- projetweb/js/avis.js
+++ projetweb/js/avis.js
@@ -1,6 +1,6 @@
 function afterSubmit() {
     const img = document.getElementById("aftersubmit");
-    const form = document.getElementById("formular");
+    const form = document.getElementById("draggableForm");
     form.style.display = "none";
     img.style.display = "block";
 
